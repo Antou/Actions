d7e0e76a7ce762cd01cc368c1bd2471f52a92fb8
abxu <shyw59@gmail.com>
2015-10-16 06:02:55 +0200

    add ActionTest.java

---
+++ test/pool/actions/ActionTest.java
@@ -0,0 +1,26 @@
+package pool.actions;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pool.actions.exceptions.ActionFinishedException;
+
+public abstract class ActionTest {
+	
+	protected abstract Action createAction();
+	
+	@Test(expected = ActionFinishedException.class, timeout = 2000)
+	public void doStepWhileFinishedThrowsException() throws ActionFinishedException {
+		Action action = createAction();
+		while (!action.isFinished()) {
+			try {
+				action.doStep();
+			} catch (ActionFinishedException e) {
+				fail("action was not supposed to be finished, we just checked");
+			}
+		}
+		assertTrue(action.isFinished());
+		action.doStep();
+	}
+}
