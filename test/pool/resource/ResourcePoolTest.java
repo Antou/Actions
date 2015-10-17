4c43f4ce24d7c3558043860b0bb7f01b893b50af
abxu <shyw59@gmail.com>
2015-10-17 15:25:28 +0200

    fix ResourcePoolTest.java

--- test/pool/resource/ResourcePoolTest.java
+++ test/pool/resource/ResourcePoolTest.java
@@ -11,31 +11,38 @@ public class ResourcePoolTest {
 	@Test 
 	public void NoSuchElementExceptionTest() {
 		ResourcePool<Basket> baskets= new BasketPool(0);
+		
 		try {
 			baskets.provideResource();
 		    fail("Should throw exception when no resource is available");
-		  }catch(NoSuchElementException exp){
-		    assert(exp.getMessage().contains("No resource"));
-		  }		
+		}
+		catch(NoSuchElementException exp) {
+			assert(exp.getMessage().contains("No resource"));
+		}	
 	}
 	
 	@Test 
 	public void IllegalArgumentExceptionTest() {
 		ResourcePool<Basket> baskets= new BasketPool(0);
 		Basket basket = new Basket();
+		
 		try {
 			baskets.freeResource(basket);
 		    fail("Should throw exception when the resource is not managed by this ResourcePool");
-		  }catch(IllegalArgumentException Exp){
-		    assert(Exp.getMessage().contains("not included"));
-		  }		
+		}
+		catch(IllegalArgumentException exp) {
+			assert(exp.getMessage().contains("not included"));
+		}
 	}
 	
 	@Test
 	public void provideResourceTest() throws NoSuchElementException {
 		ResourcePool<Cubicle> cubicles = new CubiclePool(2);
-		assertEquals(cubicles.resources.get(0),cubicles.provideResource());
-		assertEquals(cubicles.resources.get(1),cubicles.provideResource());
+		Cubicle cubicle1 = cubicles.resources.get(0);
+		Cubicle cubicle2 = cubicles.resources.get(1);
+		
+		assertEquals(cubicle1, cubicles.provideResource());
+		assertEquals(cubicle2, cubicles.provideResource());
 	}
 	
 	@Test
