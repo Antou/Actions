53fb9c94bca6c350a841a057c17931845ffb68ef
abxu <shyw59@gmail.com>
2015-10-19 14:14:58 +0200

    resourcefuluser test

---
+++ test/pool/resource/ResourcefulUserTest.java
@@ -0,0 +1,42 @@
+package pool.resource;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ResourcefulUserTest {
+	
+	@Test
+	public void testSetResource() {
+		ResourcefulUser<Cubicle> resourcefulUserTest = new ResourcefulUser<Cubicle>();
+		Cubicle resourceTest = new Cubicle();
+		
+		resourcefulUserTest.setResource(resourceTest);
+		assertEquals(resourcefulUserTest.resource, resourceTest);
+	}
+	
+	@Test
+	public void testResetResource() {
+		ResourcefulUser<Cubicle> resourcefulUserTest = new ResourcefulUser<Cubicle>();
+		Cubicle resourceTest = new Cubicle();
+		
+		resourcefulUserTest.setResource(resourceTest);
+		assertEquals(resourcefulUserTest.resource, resourceTest);
+		
+		resourcefulUserTest.resetResource();
+		
+		assertNull(resourcefulUserTest.resource);	
+	}
+	
+	@Test
+	public void testGetResource() {
+		ResourcefulUser<Basket> resourcefulUserTest = new ResourcefulUser<Basket>();
+		Basket resourceTest = new Basket();
+		
+		resourcefulUserTest.setResource(resourceTest);
+		
+		assertEquals(resourcefulUserTest.resource, resourcefulUserTest.getResource());
+		assertEquals(resourceTest, resourcefulUserTest.getResource());
+	}
+
+}
