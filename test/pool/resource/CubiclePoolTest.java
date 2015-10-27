0a8175add5ed68610941cefb7ddc859a66d952c2
abxu <shyw59@gmail.com>
2015-10-27 19:26:44 +0100

    Update CubiclePoolTest.java
    bad comment

--- test/pool/resource/CubiclePoolTest.java
+++ test/pool/resource/CubiclePoolTest.java
@@ -18,7 +18,7 @@ public class CubiclePoolTest extends ResourcePoolTest<Cubicle> {
 	public void testCreateResource() {
 		CubiclePool cubiclePool = new CubiclePool(1);
 		@SuppressWarnings("unused")
-		// this test only makes sure that createResource() returns a Basket
+		// this test only makes sure that createResource() returns a Cubicle
 		// instance. If the test compiles, the test must pass.
 		Cubicle cubicle = cubiclePool.createResource();
 	}
